ab670fdb1f0f445e558a5f7dddd06b568b253fb2
PravinSelva5 <selvarap@mcmaster.ca>
2020-07-08 11:04:38 -0400

    updated buy_and_sell_stock.py file

--- buy_and_sell_stock.py
+++ buy_and_sell_stock.py
@@ -13,4 +13,15 @@ class Solution:
                 max_profit = prices[i] - minimum_buy
         return max_profit
         
-        
+'''
+Another Successful code entry ... 124ms
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int: 
+        max_profit = 0
+        min_buy = 10000
+        for price in prices:
+            min_buy = min(min_buy, price)
+            max_profit = max(max_profit, price - min_buy)
+        return max_profit
+'''
