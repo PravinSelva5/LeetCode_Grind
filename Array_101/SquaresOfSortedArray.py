7599d8f4e5783a47b9fda278bbb1168a340a6d8f
PravinSelva5 <selvarap@mcmaster.ca>
2020-08-12 00:02:57 -0400

    Added another solution for Squares of Sorted Array

--- Array_101/SquaresOfSortedArray.py
+++ Array_101/SquaresOfSortedArray.py
@@ -1,3 +1,7 @@
+'''
+SOLUTION 1
+
+
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
         squared_array  = []
@@ -6,3 +10,29 @@ class Solution:
             squared_array.append(A[i] ** 2)
         
         return (sorted(squared_array))
+'''
+
+'''
+SOLUTION 2
+'''
+
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        
+        length = len(A)
+        i =  0
+        j = length - 1
+        
+        output = [0] * length
+        
+        while (i < length and j >= i):
+            anchor = j - i
+            if abs(A[i]) > A[j]:
+                output[anchor] = A[i] ** 2
+                i += 1
+            
+            else:
+                output[anchor] = A[j] ** 2
+                j -= 1
+        
+        return output
