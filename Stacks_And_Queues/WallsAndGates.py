f89c4f29c9a1ba8e0f3bc1464dbc029d50f4dbb0
PravinSelva5 <selvarap@mcmaster.ca>
2020-08-28 16:05:16 -0400

    worked on walls and gates question

--- Stacks_And_Queues/WallsAndGates.py
+++ Stacks_And_Queues/WallsAndGates.py
@@ -8,3 +8,47 @@ INF - Infinity means an empty room. We use the value 231 - 1 = 2147483647 to rep
 Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
 '''
 
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        
+        # Check if the rooms are empty
+        if rooms == None or rooms == []:
+            return
+        
+        # Get the rows and columns of the rooms list
+        rows = len(rooms)
+        columns = len(rooms[0])
+        queue = []
+        empty = 2147483647 
+        grid = [[1,0], [0,1], [-1,0], [0,-1]]
+        
+        # find coordinates of where gates are present
+        
+        for row in range(rows):
+            for column in range(columns):
+                if rooms[row][column] == 0:
+                    queue.append([row,column])
+                    
+        # BFS algorithm
+        
+        while queue:
+            # Pop out coordinates of first gate
+            r, c = queue.pop(0)
+            
+            
+            for x,y in grid:
+                # Check around the gate if there any empty cells
+                delta_x = x + r
+                delta_y = y + c
+                
+                # Avoiding going out of bounds
+                if delta_x < 0 or delta_y < 0 or delta_x >= rows or delta_y >= columns:
+                    continue
+                
+                # Replace empty room with distance value and add its position to queue to be checked
+                if rooms[delta_x][delta_y] == empty:
+                    rooms[delta_x][delta_y] = rooms[r][c] + 1
+                    queue.append([delta_x,delta_y])
