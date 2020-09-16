00454f39d5d07e8e20cf1b2f6e3a412ebc7d7aaa
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-16 00:54:19 -0400

    need to re-do Longest Common Prefix LeetCode Question

--- Array__And_Strings/LongestCommonPrefix.py
+++ Array__And_Strings/LongestCommonPrefix.py
@@ -3,6 +3,9 @@ Write a function to find the longest common prefix string amongst an array of st
 
 If there is no common prefix, return an empty string "".
 
+
+
+RE DO THIS QUESTION
 '''
 
 class Solution:
