07fd52449e690a9be7b7d786b42d6996d76fdbed
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-11-25 14:27:41 -0500

    added a cleaner solution for Find First and Last Position of Element in Sorted Array

--- FindFirstAndLastPositionOfElementInSortedArray.py
+++ FindFirstAndLastPositionOfElementInSortedArray.py
@@ -70,3 +70,75 @@ class Solution:
 s = Solution()
 answer = s.searchRange([1], 1)
 print(answer)
+
+
+'''
+11/25/2020 14:24	Accepted	80 ms	15.3 MB	python3
+
+class Solution:
+    
+    def findLeftPosition(self, nums, target):
+        start_ptr = 0
+        end_ptr = len(nums) - 1
+        left = 0
+        
+        while start_ptr <= end_ptr:
+            
+            midpoint = (start_ptr + end_ptr) // 2
+            
+            if nums[midpoint] == target:
+                
+                if (midpoint - 1 >= 0 and nums[midpoint - 1] != target) or midpoint == 0:
+                    
+                    left = midpoint
+                
+                end_ptr = midpoint - 1
+            
+            elif nums[midpoint] > target:
+                end_ptr = midpoint - 1
+            
+            else:
+                start_ptr = midpoint + 1
+        return left
+    
+    def findRightPosition(self,nums, target):
+        # Finding last position of target
+        start_ptr = 0
+        end_ptr = len(nums) - 1
+        right = 0
+        while start_ptr <= end_ptr:
+            midpoint = (start_ptr + end_ptr) // 2
+            if nums[midpoint] == target:
+                
+                if midpoint == len(nums) - 1 or nums[midpoint + 1] != target:
+                    right = midpoint
+                    
+                start_ptr = midpoint + 1
+            
+            elif nums[midpoint] > target:
+                end_ptr = midpoint - 1
+            else:
+                start_ptr = midpoint + 1
+        
+        return right
+        
+    
+    
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # Array is sorted, think about binary search!
+        # Time Complexity: log(N) because I'm using binary search
+        # Space Complexity: O(1), no additional memory is created, Just constants such as         # the pointers
+        # Find position of the first occurence first
+        
+        # Check if target is in array
+        if (target in nums) == False:
+            return [-1,-1]
+        
+        left = self.findLeftPosition(nums, target)
+        right = self.findRightPosition(nums, target)
+        
+        return [left, right]
+
+        
+'''
