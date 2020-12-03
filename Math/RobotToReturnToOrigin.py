ef30d675e049beefc3b23ba64d832f21f5b8485c
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-12-03 13:16:44 -0500

    made some updates in robot to return to origin script

--- Math/RobotToReturnToOrigin.py
+++ Math/RobotToReturnToOrigin.py
@@ -1,9 +1,15 @@
 '''
 There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
 
-The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). 
+If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
 
-Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+Note: The way that the robot is "facing" is irrelevant. "R" will always make the robot move to the right once, "L" will always make it move left, etc. 
+Also, assume that the magnitude of the robot's movement is the same for each move.
+
+Runtime: 68 ms, faster than 29.76% of Python3 online submissions for Robot Return to Origin.
+Memory Usage: 14.4 MB, less than 29.71% of Python3 online submissions for Robot Return to Origin.
+
 '''
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
