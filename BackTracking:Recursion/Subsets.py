a8f67080fed5f390f402b2b48141a07886f317a1
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-12-11 23:56:35 -0500

    trying to understand recursion & backtracking questions

---
+++ BackTracking:Recursion/Subsets.py
@@ -0,0 +1,10 @@
+'''
+Given an integer array nums, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets.
+
+*** 
+What is a subset?
+***
+A set X is a subset of another set Y if all elements of the set X are elements in the set Y
+''' 
+
