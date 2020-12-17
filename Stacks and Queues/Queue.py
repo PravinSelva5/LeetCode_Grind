29d4308325580696ae726b79760f9e8e29f4a305
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-12-17 14:55:31 -0500

    queue implementation included

--- Stacks and Queues/Queue.py
+++ Stacks and Queues/Queue.py
@@ -1,4 +1,25 @@
 '''
 - Linear data structure that has the order of First In First Out (FIFO)
 
+- Enqueue, Front, Dequeue all have O(1)
 '''
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, input):
+        self.queue.append(input)
+    
+    def front(self):
+        if len(self.queue) > 0:
+            return self.queue[0]
+        else:
+            return "Empty Queue"
+    def dequeue(self):
+        if len(self.queue) != 0:
+            self.queue.pop(0)
+        else:
+            return "Empty Queue"
+
