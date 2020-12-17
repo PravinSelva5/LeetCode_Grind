d369088a5c532aa5a8898fff96e36b227141419e
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-12-17 14:53:34 -0500

    stack and queue implementation

---
+++ Stacks and Queues/Queue.py
@@ -0,0 +1,4 @@
+'''
+- Linear data structure that has the order of First In First Out (FIFO)
+
+'''
---
+++ Stacks/stacks.py
@@ -0,0 +1,33 @@
+'''
+- Linear data structure which has the order of last in first out (LIFO)
+- pop removes the top element from the stack and shows the value 
+- top shows the value of the top element
+- push, pushes a new element to the stack (array)
+
+----------------------------------------------------------------
+Time Complexity for pop, push, and top for a stack are all O(1)
+----------------------------------------------------------------
+
+
+
+'''
+
+class PlateStack:
+
+    def __init__(self):
+        self.stack = []
+    
+    def push(self,x ):
+        self.stack.append(x)
+    
+    def pop(self):
+        if len(self.stack > 0):
+            self.stack.pop()
+        else:
+            return "Empty Stack"
+
+    def top(self):
+        if len(self.stack > 0):
+            return self.stack[-1]
+        else:
+            return "Empty Stack"
