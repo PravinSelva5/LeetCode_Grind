b1bc791520473b73bf5b621303ff0d5bdce23787
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-12-21 14:54:43 -0500

    completed letter combinations of a phone number question

--- BackTracking:Recursion/LetterCombinationsofaPhoneNumber.py
+++ BackTracking:Recursion/LetterCombinationsofaPhoneNumber.py
@@ -2,10 +2,30 @@
 Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
 A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+--------------
+    RESULTS
+--------------
+
+Runtime: 24 ms, faster than 94.81% of Python3 online submissions for Letter Combinations of a Phone Number.
+Memory Usage: 14.4 MB, less than 18.95% of Python3 online submissions for Letter Combinations of a Phone Number.
+
 '''
 
 class Solution:
-    def backtrack(self,)
+    def backtrack(self,ans, numberPad, digits, combination, index ):
+        if index > len(digits):
+            return
+        
+        if len(combination) == len(digits):
+            ans.append(combination[:])
+            return
+        
+        currentDigit = digits[index]
+        currentString = numberPad[currentDigit]
+        
+        for i in range(len(currentString)):
+            self.backtrack(ans, numberPad, digits, combination+currentString[i], index+1)
     
     
     
@@ -14,8 +34,8 @@ class Solution:
         if digits == "":
             return ans
         
-        numberPad = { 2: "abc", 3: "def", 4: "ghi", 5:"jkl", 6:"mno", 7"pqrs", 8:"tuv", 9: "wxyz" }
+        numberPad = { '2': "abc", '3': "def", '4': "ghi", '5':"jkl", '6':"mno", '7':"pqrs", '8':"tuv", '9': "wxyz" }
         
-        backtrack(ans, m, digits, combination="", index=0)
+        self.backtrack(ans, numberPad, digits, "", 0)
         
         return ans
