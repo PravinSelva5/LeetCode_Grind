b164d7444bfa50abd3edf40e571d046a12664e47
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-12-23 22:53:57 -0500

    working on a BFS implementation

---
+++ BFS/BFS.py
@@ -0,0 +1,10 @@
+'''
+Breadth First Search
+
+ - Searching algorithm
+ - Traverses graph layer/level by layer/level
+ - Moves horizontally and visits all nodes in the same layer before moving to the next layer.
+ - You can use QUEUES to achieve this
+
+
+'''
